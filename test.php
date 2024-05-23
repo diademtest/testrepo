5c5ee5855b5baa68e699fe746eb440ff7d147ace
diademtest <rakib@diadem.in>
2024-05-23 16:26:59 +0530

    Create test.php

---
+++ test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello, World!</title>
+</head>
+<body>
+
+<?php
+    // PHP code to display "Hello, World!"
+    echo "<h1>Hello, World!</h1>";
+?>
+
+</body>
+</html>
